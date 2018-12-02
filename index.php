1490ea720100b3691d1ba25be7879a6db625c926
Zhaohe <zzhan725@uwo.ca>
2018-12-02 07:45:20 -0500

    add index and connect

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home Page</title>
+</head>
+<body>
+<?php
+include 'connectdb.php';
+?>
+<h1>Welcome to the Sales Information Database</h1>
+<h2>Customer Information</h2>
+</body>
+</html>
