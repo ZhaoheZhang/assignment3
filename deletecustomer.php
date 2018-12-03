851a7595772cb040de470df004136391fb19f3b0
Zhaohe <zzhan725@uwo.ca>
2018-12-03 16:57:18 -0500

    add cus

---
+++ deletecustomer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Delete Customer</title>
+</head>
+<body>
+<?php
+include 'connectdb.php';
+?>
+<h1>Deleting Customer:</h1>
+<ol>
+<?php
+   $whichCustomer= $_POST["customer"];
+   $query = 'DELETE FROM customer WHERE customer.customerid ="'.$whichCustomer.'"';
+   $result=mysqli_query($connection,$query);
+    if (!$result) {
+         die("database query2 failed.");
+     }
+     echo "Customer deleted!";
+     mysqli_free_result($result);
+?>
+</ol>
+<?php
+   mysqli_close($connection);
+?>
+</body>
+</html>
