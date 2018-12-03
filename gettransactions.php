dc4ce17906ae0d9bd0b5e350e8864f6491aef86d
Zhaohe <zzhan725@uwo.ca>
2018-12-03 02:56:12 -0500

    purchase

--- gettransactions.php
+++ gettransactions.php
@@ -12,7 +12,7 @@ include 'connectdb.php';
 <ol>
 <?php
    $whichCustomer= $_POST["customer"];
-   $query = 'SELECT customer.firstname, customer.lastname, product.description deal.quantity FROM customer, product, deal WHERE product.productid = deal.productid AND customer.customerid = deal.customerid AND customer.customerid ="'.$whichCustomer.'"';
+   $query = 'SELECT customer.firstname, customer.lastname, product.description, deal.quantity  FROM customer, product, deal WHERE product.productid = deal.productid AND customer.customerid = deal.customerid AND customer.customerid ="'.$whichCustomer.'"';
    $result=mysqli_query($connection,$query);
     if (!$result) {
          die("database query2 failed.");
