ae32e1c93d9513cf70f7660b96564c27c1d3df7c
Zhaohe <zzhan725@uwo.ca>
2018-12-03 16:07:04 -0500

    add cus

---
+++ getphone.php
@@ -0,0 +1,15 @@
+<?php
+   $query = "SELECT * FROM customer ORDER BY lastname";
+   $result = mysqli_query($connection,$query);
+   if (!$result) {
+        die("databases query failed.");
+    }
+   echo "Choose the customer </br>";
+   while ($row = mysqli_fetch_assoc($result)) {
+        echo '<input type="radio" name="customer" value="';
+        echo $row["customerid"];
+        echo '">' . $row["firstname"] . " " . $row["lastname"] . ": " . $row["phone"] . "<br>";
+   }
+   mysqli_free_result($result);
+?>
+
