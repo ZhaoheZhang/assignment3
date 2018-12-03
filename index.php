e08912707378e7f45420061cb9870f3e670042a2
Zhaohe <zzhan725@uwo.ca>
2018-12-03 17:38:57 -0500

    add cus

--- index.php
+++ index.php
@@ -83,9 +83,6 @@ include 'getdata.php';
 <form action="selectcustomer.php" method="post" enctype="multipart/form-data">
 <h2>Valuable Customers</h2>
 Show customer who has pruchased more than <input type="text" name="quantity"> (quantity) of any item<br>
-<?php
-include 'selectcustomer.php';
-?>
 </form>
 <?php
 mysqli_close($connection);
