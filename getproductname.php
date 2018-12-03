a0d4a70d3d6cae2c82a4818e44fb587923a80540
Zhaohe <zzhan725@uwo.ca>
2018-12-03 02:03:51 -0500

    purchase

--- getproductname.php
+++ getproductname.php
@@ -8,7 +8,7 @@
    while ($row = mysqli_fetch_assoc($result)) {
         echo '<input type="radio" name="product" value="';
         echo $row["productid"];
-        echo '">' . $row["description"] . " quantity: " . $row["quantity"] . "<br>";
+        echo '">' . $row["description"] . "<br>";
    }
    mysqli_free_result($result);
 ?>
