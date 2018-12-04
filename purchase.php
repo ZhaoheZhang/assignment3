c72ac7a8cad96b44959ec639f52c22aa88969b4d
Zhaohe <zzhan725@uwo.ca>
2018-12-03 21:37:45 -0500

    sales detail

--- purchase.php
+++ purchase.php
@@ -14,7 +14,7 @@
    $purchasedbefore = 0;
    $whichcustomer= $_POST["customer"];
    $productid = $_POST["product"];
-   $purchasequantity =$_POST["quantity"];
+   $quantity =$_POST["quantity"];
    $query1 = 'SELECT product.quantity FROM product WHERE product.productid = '.$productid.'';
    $result1 = mysqli_query($connection,$query1);
    if (!$result1) {
@@ -23,12 +23,33 @@
     $row1 = mysqli_fetch_assoc($result1);
     $stock = $row1["quantity"];
 
-    if($purchasequantity > $stock) {
+    if($quantity > $stock) {
       echo "Unable to purchase, item out of stock.";
     }
     else {
-      echo "Purchase Success";
+      $query2='SELECT * FROM deal';
+      $result2=mysqli_query($connection,$query2);
+      if (!$result2) {
+        die("databases query2 failed.");
+      }
+      while($row=mysqli_fetch_assoc($result2)) {
+        if($row["customerid"] == $whichcustomer and $row["productid"] == $productid) {
+          $purchasedbefore = 1;
+          $quantity = $quantity + row["quantity"];
+        }
+      }
+      if($purchasedbefore == 0) {
+        $query3='INSERT INTO deal VALUES('.$quantity.', "'.$whichcustomer.'", "'.$productid.'")';
+      }
+      elseif($purchasedbefore == 1) {
+        $query3='UPDATE deal SET quantity='.$quantity.' WHERE customerid="'.$whichcustomer.'" AND productid="'.$productid.'" ';
+      }
+      if(!mysqli_query($connection,$query3)) {
+        die("Cannot insert".mysqli_error($connection));
+      }
     }
+   mysqli_free_result($result1);
+   mysqli_free_result($result2);
    mysqli_close($connection);
 ?>
 </ol>
