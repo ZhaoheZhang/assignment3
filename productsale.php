fa22706bff5be4e474d6ae11340fe73f9257971d
Zhaohe <zzhan725@uwo.ca>
2018-12-03 20:20:16 -0500

    sales detail

--- productsale.php
+++ productsale.php
@@ -18,7 +18,11 @@ include 'connectdb.php';
          die("database query failed.");
      }
     $row=mysqli_fetch_assoc($result);
-    echo '<li>';
+
+
+    echo $row["description"];
+
+
     echo $row["totalNum"] . " " . $row["description"] . "have been sold, make " . $row["totalNum"]*$row["cost"] . " profit";
     mysqli_free_result($result);
 ?>
